b845e5b5135edabe44b69acf5798adc205fdf5c3
Jamie Mason <jamie.mason@socc.edu>
2024-12-29 23:45:40 +0000

    undo test changes

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Author: Jamie
+ * Author: [Your name here]
  * Last Modified: [Current date]
  * Description: Hello World program for Lab 1
  * Input: N/A
@@ -12,6 +12,6 @@ using namespace std;
 
 int main() {
     // INSTRUCTIONS: Change the message below to print anyting you like other than  "Hello, Codespaces!"
-    cout << "Hello, Codespaces! test" << endl;
+    cout << "Hello, Codespaces!" << endl;
     return 0;
 } 
